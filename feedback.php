278368d333fd5f62688f1e16086ea6cca2351e72
Harold Thimbleby <harold@thimbleby.net>
2026-05-23 19:50:59 +0100

    Improved feedback

--- feedback.php
+++ feedback.php
@@ -13,7 +13,7 @@
             $version = urldecode($_GET['version']);
             $compiled = urldecode($_GET['compiled']);
             
-            $record = "$date\t$author\t$email\t$version\t$compiled\t$feedback\n****\n";
+            $record = "<tr><td>$date</td><td>$author</td><td>$email</td><td>$version</td><td>$compiled</td><td style=\"color:firebrick\">$feedback</td></tr>\n";
             $email = $email != ""? "email: $email": "[no email supplied]";
             
             $feedbackfd = fopen($feedbackFileName, "a+"); 
@@ -30,8 +30,12 @@
                 fclose($feedbackfd);
             }
 
-			echo "<h1>REED version ". htmlspecialchars($version)." feedback given ". htmlspecialchars($date)."</h1>". htmlspecialchars($feedback)."<p/>&mdash; from: ". htmlspecialchars($author)."<br/>&mdash; ". htmlspecialchars($email);
-            echo "<p/><hr/>Thank you for supporting REED. Do email <tt>harold@thimbleby.net</tt> with any criticisms or ideas.<p/><hr/><p/>";
+			echo "<h1>REED version ". htmlspecialchars($version)." feedback given ". htmlspecialchars($date)."</h1><p style=\"color:firebrick\">". htmlspecialchars($feedback)."<p/>&mdash; from: ". htmlspecialchars($author)."<br/>&mdash; ". htmlspecialchars($email);
+            echo "<p/><hr/>Thank you for supporting REEDs! Do email <a href=\"mailto:harold@thimbleby.net\"><tt>harold@thimbleby.net</tt></a> with any ideas or criticisms.<p/><hr/><p/>";
+            
+            $sent = mail('harold@thimbleby.net', 'REED feedback', "$date\n$author\n$email\n$feedback\n");
+            
+            echo "Sent email $sent ...</br>";
         ?>
     </body>
 </html>
